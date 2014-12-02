45ff7465efb4ba26077049abc8e63761e82ed17d
Fredy Schlag <fredy.schlag@gmail.com>
2014-12-01 23:53:21 -0200

    Projeto para testes de build e execução

---
+++ fjp-sd-test/src/test/CreateFileTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.io.File;
+import java.io.IOException;
+
+import main.CreateFile;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CreateFileTest {
+	
+	@Test
+	public void test01() throws IOException {
+		File file = new File("C:\\temp\\fjp-sd-test-01.txt");		
+		file.delete();
+		Assert.assertFalse(file.exists());
+		CreateFile createFile = new CreateFile(file);
+		createFile.execute();
+		Assert.assertTrue(file.exists());
+	}
+
+}
