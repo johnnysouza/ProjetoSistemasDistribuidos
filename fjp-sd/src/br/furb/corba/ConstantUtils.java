372c165d2d93963d9a3c2978cd6c0bb35f378d0b
Jonathan <johnnysouza3@gmail.com>
2014-12-02 18:48:57 -0200

    Começando implementação do cliente do Corba

--- fjp-sd/src/br/furb/corba/ConstantUtils.java
+++ fjp-sd/src/br/furb/corba/ConstantUtils.java
@@ -5,5 +5,6 @@ public class ConstantUtils {
 	public static final String SERVICE_NAME = "JobManager";
 	public static final String JOB_HISTORY = "history";
 	public static final String DATE_FORMAT = "dd-MM-yyyy_HH-mm-ss-SSSS";
+	public static final String[] CORBA_SERVER = new String[]{"-ORBInitialHost", ""};
 
 }
--- fjp-sd/src/br/furb/corba/JobManagerClient.java
+++ fjp-sd/src/br/furb/corba/JobManagerClient.java
@@ -7,31 +7,52 @@ import org.omg.CosNaming.NamingContextExtHelper;
 import org.omg.CosNaming.NamingContextPackage.CannotProceed;
 import org.omg.CosNaming.NamingContextPackage.NotFound;
 
+import br.furb.corba.configuration.Job;
 import br.furb.corba.configuration.history.JobHistory;
+import br.furb.corba.configuration.stubs.History_Status;
+import br.furb.corba.configuration.stubs.Job_History;
+import br.furb.corba.configuration.stubs.arrayJobsHistoryHolder;
 import br.furb.corba.configuration.stubs.job_manager;
 import br.furb.corba.configuration.stubs.job_managerHelper;
 
 public class JobManagerClient {
 	
-	public static void main(String[] args) {
-		try {
-		// Cria e inicializa o ORB
-	      ORB orb = ORB.init(args, null);
+	private job_manager jobManager;
 
-	      // Obtem referencia para o servico de nomes
-	      org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
-	      NamingContextExt namecontextRef = NamingContextExtHelper.narrow(objRef);
-	 
-	      // Obtem referencia para o servidor
-	      String name = ConstantUtils.SERVICE_NAME;
-	      job_manager jobManager = job_managerHelper.narrow(namecontextRef.resolve_str(name));
+	public JobManagerClient(String[] args) {
+		try {
+			// Cria e inicializa o ORB
+			ORB orb = ORB.init(args, null);
+
+			// Obtem referencia para o servico de nomes
+			org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+			NamingContextExt namecontextRef = NamingContextExtHelper.narrow(objRef);
+
+			// Obtem referencia para o servidor
+			String name = ConstantUtils.SERVICE_NAME;
+			jobManager = job_managerHelper.narrow(namecontextRef.resolve_str(name));
 		} catch (InvalidName | NotFound | CannotProceed | org.omg.CosNaming.NamingContextPackage.InvalidName ex) {
 			System.out.println("");
 		}
 	}
+
+	public void addHistory(String jobName, JobHistory history) {
+		History_Status status = History_Status.from_int(history.getStatus().ordinal());
+		Job_History historyAdapter = new Job_History(history.getDateInMillis(), history.getLog(), status);
+		jobManager.add_history(jobName, historyAdapter);
+	}
 	
-	public static void addHistory(String jobName, JobHistory history) {
-		//TODO Chamar o método via corba
+	public JobHistory[] loadHistorys(String jobName) {
+		arrayJobsHistoryHolder historys = new arrayJobsHistoryHolder();
+		jobManager.loadHistorys(jobName, historys);
+		Job_History[] historysAdapter = historys.value;
+		//TODO
+		return null;
+	}
+	
+	public Job[] loadAll() {
+		//TODO
+		return null;
 	}
 
 }
